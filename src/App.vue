d4eda3b1de8fb19868d09888a47edfecac7d3e6f
sidereior <alexanderlnanda@gmail.com>
2023-08-06 23:33:52 -0400

    placeholder all done

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@
         :description="item.description"
         :imageSrc="item.imageSrc"
         :imagePosition="(index % 2 === 0) ? 'left' : 'right'"
+        :parallaxBackground="item.parallaxBackground"
       />
     </div>
   </div>
@@ -25,96 +26,80 @@ export default {
   data() {
     return {
       items: [
-        {
+      {
           title: "Class 1",
           description: "Description of Class 1",
-          imageSrc: require('@/assets/class1.png'), // Local image path
+          imageSrc: require('@/assets/class1.png'),
+          parallaxBackground: require('@/assets/class1.png'), 
         },
         {
           title: "Project 1",
           description: "Description of Project 1",
-          imageSrc: require('@/assets/project1.png'), // Local image path
+          imageSrc: require('@/assets/project1.png'), 
+          parallaxBackground: require('@/assets/project1.png'), 
         },
         {
           title: "Job Experience 1",
           description: "Description of Job Experience 1",
-          imageSrc: require('@/assets/job1.png'), // Local image path
-        },
-        // Add more items as needed
-      ],
+          imageSrc: require('@/assets/job1.png'), 
+          parallaxBackground: require('@/assets/job1.png'), 
+        },  
+    ],
     };
-  },
+  }
 };
 </script>
 
-
 <style>
-#app {
-  /* Add your global styles for the entire app here */
-  font-family: 'Your-Font-Family', sans-serif;
-  color: #fff;
+
+.list-item {
+  width: 100vw; 
+  height: 500px;
+  display: flex;
+  align-items: center;
+  margin: 20px 0;
+  position: relative;
+  overflow: hidden;
 }
 
-.parallax-background {
-  /* Add your parallax background styles here */
-  /* For example, you can use a background image and adjust background-position */
-  background-image: url('https://upload.wikimedia.org/wikipedia/commons/9/97/Lockheed_SR-71_Blackbird.jpg');
-  background-size: cover;
-  background-attachment: fixed;
-  height: 100vh;
+#app {
+  background: #000;
+  color: #e0e0e0;
+  font-family: 'Your-Font-Family', sans-serif;
 }
 
 .list {
-  /* Add your styles for the list container here */
-  /* For example, you can use flexbox to center the items */
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   padding: 20px;
+  overflow: hidden;
 }
 
 .list-item {
-  /* Add your styles for each list item here */
-  /* For example, you can use flexbox to arrange the elements */
   display: flex;
-  flex-direction: row;
   align-items: center;
   margin: 20px 0;
-}
-
-.content {
-  /* Add your styles for the item content here */
-  /* For example, you can set the width and padding */
-  width: 80%;
-  padding: 20px;
-}
-
-.image {
-  /* Add your styles for the image container here */
-  /* For example, you can set the width and add a border */
-  width: 40%;
-  border: 2px solid #fff;
+  position: relative;
+  overflow: hidden;
 }
 
 .text {
-  /* Add your styles for the text container here */
-  /* For example, you can set the width and add some spacing */
   width: 60%;
   padding: 0 20px;
 }
 
+
+
 h2 {
-  /* Add your styles for the title here */
-  /* For example, you can set the font size and add a shadow effect */
   font-size: 24px;
-  text-shadow: 2px 2px 4px #000;
+  text-shadow: 0 0 10px #32CD32, 0 0 20px #32CD32;
 }
 
 p {
-  /* Add your styles for the description here */
-  /* For example, you can set the font size and add some spacing */
   font-size: 16px;
   margin-top: 10px;
+  text-shadow: 0 0 10px #32CD32, 0 0 20px #32CD32;
 }
 </style>
--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -1,9 +1,9 @@
-<!-- ListItem.vue -->
 <template>
-  <div class="list-item">
+  <div class="list-item" :style="parallaxStyle">
+    <div class="parallax-overlay"></div>
     <div class="content">
       <div class="image" :class="imagePosition">
-        <img :src="require('@/assets/class1.png')" alt="Item Image">
+        <img :src="imageSrc" alt="Item Image">
       </div>
       <div class="text">
         <h2>{{ title }}</h2>
@@ -19,11 +19,51 @@ export default {
     title: String,
     description: String,
     imageSrc: String,
-    imagePosition: String, // 'left' or 'right'
+    imagePosition: String,
+    parallaxBackground: String, 
+  },
+  computed: {
+    parallaxStyle() {
+      return {
+        background: this.parallaxBackground
+          ? `url(${this.parallaxBackground}) center/cover fixed no-repeat`
+          : "transparent",
+      };
+    },
   },
 };
 </script>
 
+
 <style>
-/* Add your styles for each item here */
+
+.image {
+  width: 40%;
+  border: 2px solid #fff;
+}
+
+.text {
+  width: 60%;
+  padding: 0 20px;
+}
+
+.list-item {
+  position: relative;
+  z-index: 0; /* Newly added */
+}
+
+.content {
+  position: relative;  
+  z-index: 2; /* Modified */
+}
+
+.parallax-overlay {
+  background: rgba(0, 0, 0, 0.5); /* Overlay with black and 50% transparency */
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1; /* Modified */
+}
 </style>
